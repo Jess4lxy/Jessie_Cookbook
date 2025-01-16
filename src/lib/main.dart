d8630b54d5f6160585baf584b9755e5a4c5fcd24
Jess4lxy <alex.mtz.rv17@gmail.com>
2025-01-16 14:22:29 -0600

    forms completado + animaciones del gato pendejo

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -19,8 +19,9 @@ class MyApp extends StatelessWidget {
         '/design': (context) => const DesignScreen(),
         '/images': (context) => const ImagesScreen(),
         '/list': (context) => const ListScreen(),
-        '/forms': (context) => const PlaceholderScreen('Forms Section'),
-        '/navigation': (context) => const PlaceholderScreen('Navigation Section'),
+        '/forms': (context) => const FormsScreen(),
+        '/navigation': (context) =>
+            const PlaceholderScreen('Navigation Section'),
       },
     );
   }
@@ -35,30 +36,30 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Jessie Cookbook')),
       body: Center(
         child: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: [
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/design'),
-            child: const Text('Design'),
-          ),
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/images'),
-            child: const Text('Images'),
-          ),
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/list'),
-            child: const Text('List'),
-          ),
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/forms'),
-            child: const Text('Forms'),
-          ),
-          ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/navigation'),
-            child: const Text('Navigation'),
-          ),
-        ],
-      ),
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/design'),
+              child: const Text('Design'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/images'),
+              child: const Text('Images'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/list'),
+              child: const Text('List'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/forms'),
+              child: const Text('Forms'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/navigation'),
+              child: const Text('Navigation'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -268,6 +269,19 @@ class ImagesScreen extends StatefulWidget {
 }
 
 class _ImagesScreenState extends State<ImagesScreen> {
+  late String image; // Declaramos la imagen como `late`.
+
+  @override
+  void initState() {
+    super.initState();
+    _resetImage();
+  }
+
+  void _resetImage() {
+    // Inicializamos la URL de la imagen.
+    image = 'https://i.kym-cdn.com/entries/icons/mobile/000/043/403/cover3.jpg';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -278,9 +292,9 @@ class _ImagesScreenState extends State<ImagesScreen> {
             return IconButton(
               icon: const Icon(Icons.arrow_back, color: Colors.black),
               onPressed: () {
-                Navigator.pop(context); // Cierra el Drawer
-                Navigator.popUntil(
-                    context, ModalRoute.withName('/')); // Cierra el Drawer
+                // Reinicia el estado y regresa al home.
+                _resetImage();
+                Navigator.pop(context);
               },
               tooltip: 'Back to home',
             );
@@ -296,9 +310,9 @@ class _ImagesScreenState extends State<ImagesScreen> {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             FadeInImage.memoryNetwork(
-                placeholder: kTransparentImage,
-                image:
-                    'https://i.kym-cdn.com/entries/icons/mobile/000/043/403/cover3.jpg'),
+              placeholder: kTransparentImage,
+              image: image,
+            ),
           ],
         ),
       ),
@@ -431,46 +445,43 @@ class _ListScreenState extends State<ListScreen> {
         title: Text(items.first),
       ),
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(items[index])
+        return ListTile(title: Text(items[index]));
+      },
+    );
+  }
+
+  Widget _buildCreateListWithSpacedItems() {
+    const items = 20;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: constraints.maxHeight),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: List.generate(
+                items,
+                (index) => _buildItemWidget('Item $index'),
+              ),
+            ),
+          ),
         );
       },
     );
   }
 
-Widget _buildCreateListWithSpacedItems() {
-  const items = 20;
-
-  return LayoutBuilder(
-    builder: (context, constraints) {
-      return SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(minHeight: constraints.maxHeight),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: List.generate(
-              items,
-              (index) => _buildItemWidget('Item $index'),
-            ),
-          ),
+  Widget _buildItemWidget(String text) {
+    return Card(
+      child: SizedBox(
+        height: 100,
+        child: Center(
+          child: Text(text),
         ),
-      );
-    },
-  );
-}
-
-Widget _buildItemWidget(String text) {
-  return Card(
-    child: SizedBox(
-      height: 100,
-      child: Center(
-        child: Text(text),
       ),
-    ),
-  );
-}
-
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -640,6 +651,149 @@ Widget _buildItemWidget(String text) {
               },
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class FormsScreen extends StatefulWidget {
+  const FormsScreen({super.key});
+
+  @override
+  State<FormsScreen> createState() => _FormsScreenState();
+}
+
+class _FormsScreenState extends State<FormsScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _matriculaController = TextEditingController();
+  final TextEditingController _carreraController = TextEditingController();
+  final TextEditingController _cuatrimestreController = TextEditingController();
+  final TextEditingController _periodoController = TextEditingController();
+
+  String _submittedData = '';
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _matriculaController.dispose();
+    _carreraController.dispose();
+    _cuatrimestreController.dispose();
+    _periodoController.dispose();
+    super.dispose();
+  }
+
+  void _handleSubmit() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _submittedData = '''
+Nombre: ${_nameController.text}
+Matrícula: ${_matriculaController.text}
+Carrera: ${_carreraController.text}
+Cuatrimestre: ${_cuatrimestreController.text}
+Periodo: ${_periodoController.text}
+        ''';
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Datos enviados correctamente')),
+      );
+    }
+  }
+
+  Widget _buildTextField({
+    required String label,
+    required TextEditingController controller,
+    required String? Function(String?) validator,
+    IconData? icon,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
+          prefixIcon: icon != null ? Icon(icon) : null,
+        ),
+        validator: validator,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Student Form')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildTextField(
+                  label: 'Name',
+                  controller: _nameController,
+                  validator: (value) =>
+                      value == null || value.isEmpty ? 'Enter your name' : null,
+                  icon: Icons.person,
+                ),
+                _buildTextField(
+                  label: 'ID',
+                  controller: _matriculaController,
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Enter your identification'
+                      : null,
+                  icon: Icons.school,
+                ),
+                _buildTextField(
+                  label: 'Program',
+                  controller: _carreraController,
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Enter your program'
+                      : null,
+                  icon: Icons.book,
+                ),
+                _buildTextField(
+                  label: 'Semester',
+                  controller: _cuatrimestreController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your current semester';
+                    }
+                    if (int.tryParse(value) == null) {
+                      return 'Only numbers are accepted';
+                    }
+                    return null;
+                  },
+                  icon: Icons.timeline,
+                ),
+                _buildTextField(
+                  label: 'Term',
+                  controller: _periodoController,
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Enter your current term'
+                      : null,
+                  icon: Icons.calendar_today,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _handleSubmit,
+                  child: const Text('Submit'),
+                ),
+                const SizedBox(height: 20),
+                if (_submittedData.isNotEmpty)
+                  Text(
+                    'Data submitted:\n$_submittedData',
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+              ],
+            ),
+          ),
         ),
       ),
     );
